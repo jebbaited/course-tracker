49b7791c3ab59729ac17e80a5317692e39feb2bf
jebbaited <kostadrapak@gmail.com>
2023-01-19 13:36:52 +0200

    structure changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,26 +5,26 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [courses, setCourses] = useState([]);
-  
+
   const loadCourses = async () => {
     try {
       const res = await fetch('/.netlify/functions/courses');
       const courses = await res.json();
       setCourses(courses);
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-  }
+  };
 
   useEffect(() => {
     loadCourses();
   }, []);
-  
+
   return (
     <div className="container mt-5">
-          <h1 className="mb-5 text-center">Course Tracker</h1>
-          <CourseForm courseAdded={loadCourses} />
-          <CourseList courses={courses} refreshCourses={loadCourses} />
+      <h1 className="mb-5 text-center">Course Tracker</h1>
+      <CourseForm courseAdded={loadCourses} />
+      <CourseList courses={courses} refreshCourses={loadCourses} />
     </div>
   );
 }
